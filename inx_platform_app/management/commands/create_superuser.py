4d5f8fd8e6e5c8f52529cd2dee1ed74604a159fb
Marco Zanella <marcozanella@gmail.com>
2024-01-27 20:52:40 +0900

    reorganized migrations - added a command create_superuser

--- inx_platform_app/management/commands/create_superuser.py
+++ inx_platform_app/management/commands/create_superuser.py
@@ -1,16 +1,19 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Create a superuser for development. Marco.zanella@inxeurope.com - dellaBiella2!'
 
     def handle(self, *args, **options):
-        username = 'Marco.zanella'
+        # username = 'Marco.zanella'
         email = 'Marco.zanella@inxeurope.com'
         password = 'dellaBiella2!'
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
+        User = get_user_model()
+
+        if not User.objects.filter(email=email).exists():
+            User.objects.create_superuser(email, password)
             self.stdout.write(self.style.SUCCESS('Superuser created successfully!'))
         else:
             self.stdout.write(self.style.SUCCESS('Superuser already exists.'))
