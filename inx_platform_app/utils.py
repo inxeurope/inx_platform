7ae0e45a75b6f9996de5df5c78a28fbf99c19edd
Marco Zanella <marcozanella@gmail.com>
2024-04-15 17:48:04 +0200

    mah

--- inx_platform_app/utils.py
+++ inx_platform_app/utils.py
@@ -51,6 +51,7 @@ def check_and_create_views_and_procs(app_folder):
             for proc_name in proc_files:
                 sql_statement = f"SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{proc_name}]') AND type in (N'P', N'PC')"
                 try:
+                    print(sql_statement)
                     cursor.execute(sql_statement)
                 except Exception as e:
                     print("Error executing SQL statement:", e)
