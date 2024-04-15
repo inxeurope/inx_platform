adbd02175356093f946c90801603cc93f6d58f47
Marco Zanella <marcozanella@gmail.com>
2024-04-15 17:28:19 +0200

    removed that ' ' from sql

--- inx_platform_app/database_scripts/views/01_view_customers.sql
+++ inx_platform_app/database_scripts/views/01_view_customers.sql
@@ -15,7 +15,7 @@ BEGIN
             cc.iso3166_1_alpha_2 as country_iso_alpha2,
             cc.iso3166_1_alpha_3 as country_iso_alpha3,
             cc.official_name_en as country_name,
-            u.first_name + ' ' + u.last_name as sales_manager
+            u.first_name + u.last_name as sales_manager
         FROM inx_platform_app_customer c
         LEFT JOIN inx_platform_app_countrycode cc ON cc.id = c.country_id
         LEFT JOIN inx_platform_app_user u ON u.id = c.sales_employee_id
