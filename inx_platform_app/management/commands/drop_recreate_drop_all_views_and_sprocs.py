60446072a39164df2c01494edf657c784ed6f399
Marco Zanella <marcozanella@gmail.com>
2024-04-28 22:43:06 +0200

    updated stored procedure script _drop_all_views_and_sprocs

--- inx_platform_app/management/commands/drop_recreate_drop_all_views_and_sprocs.py
+++ inx_platform_app/management/commands/drop_recreate_drop_all_views_and_sprocs.py
@@ -13,17 +13,17 @@ class Command(BaseCommand):
             cursor.execute("""
                 CREATE PROCEDURE _drop_all_views_and_sprocs AS
                 BEGIN
-                    DROP PROCEDURE [dbo].[_budfordetailline_delete_sales];
-                    DROP PROCEDURE [dbo].[_budfordetailline_fill_sales];
-                    DROP PROCEDURE [dbo].[_budforline_add_triplets];
-                    DROP PROCEDURE [dbo].[_ke30_import];
-                    DROP PROCEDURE [dbo].[_ke30_import_add_new_customers];
-                    DROP PROCEDURE [dbo].[_ke30_import_add_new_products];
-                    DROP PROCEDURE [dbo].[_zaq_import];
-                    DROP VIEW [dbo].[01_view_customers];
-                    DROP VIEW [dbo].[02_view_products];
-                    DROP VIEW [dbo].[03_view_zaq];
-                    DROP VIEW [dbo].[04_view_budforsales_temporary];
+                    DROP PROCEDURE [_budfordetailline_delete_sales];
+                    DROP PROCEDURE [_budfordetailline_fill_sales];
+                    DROP PROCEDURE [_budforline_add_triplets];
+                    DROP PROCEDURE [_ke30_import];
+                    DROP PROCEDURE [_ke30_import_add_new_customers];
+                    DROP PROCEDURE [_ke30_import_add_new_products];
+                    DROP PROCEDURE [_zaq_import];
+                    DROP VIEW [01_view_customers];
+                    DROP VIEW [02_view_products];
+                    DROP VIEW [03_view_zaq];
+                    DROP VIEW [04_view_budforsales_temporary];
                 END
             """)
 
