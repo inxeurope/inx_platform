b5291f336544a56b1d13db8c324c1fd1d1121d00
Marco Zanella <marcozanella@gmail.com>
2024-09-07 14:49:02 +0200

    Made field sqlapp_id not required in ProductLine model

--- inx_platform_app/models.py
+++ inx_platform_app/models.py
@@ -130,7 +130,7 @@ class CurrencyRate(models.Model):
 
 class ProductLine(models.Model):
     name = models.CharField(max_length=50, blank=True)
-    sqlapp_id = models.IntegerField(default=0)
+    sqlapp_id = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return_value = f"{self.name}"
