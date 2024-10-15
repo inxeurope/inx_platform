f9512cd5c612c814c9284c0722a984c8699d7061
Marco Zanella <marcozanella@gmail.com>
2024-10-15 18:23:44 +0200

    update RebateAdmin

--- inx_platform_app/admin.py
+++ inx_platform_app/admin.py
@@ -679,17 +679,22 @@ class EuroExchangeRateAdmin(admin.ModelAdmin):
 
 
 class RebateAdmin(admin.ModelAdmin):
-    list_display=['id', 'get_customer_name', 'get_brand_name', 'scenario__name', 'year', 'rebate', 'is_active']
+    list_display=['id', 'get_customer_name', 'get_brand_name', 'get_scenario_name', 'year', 'rebate', 'is_active']
     
     def get_customer_name(self, obj):
         return obj.customer.name
     get_customer_name.admin_order_field = 'customer__name'
-    get_customer_name.short_description = 'Customer Name'
+    get_customer_name.short_description = 'Customer'
     
     def get_brand_name(self, obj):
-        return obj.customer.name
+        return obj.brand.name
     get_brand_name.admin_order_field = 'brand__name'
-    get_brand_name.short_description = 'Brand Name'
+    get_brand_name.short_description = 'Brand'
+
+    def get_scenario_name(self, obj):
+        return obj.scenario.name
+    get_scenario_name.admin_order_field = 'scenario__name'
+    get_scenario_name.short_description = 'Scenario'
 
 admin.site.register(ColorGroup, ColorGroupAdmin)
 admin.site.register(Color, ColorAdmin)
