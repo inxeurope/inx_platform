d57b03aee991d1035a2229a1de65e2c6c15f1ccd
Marco Zanella <marcozanella@gmail.com>
2024-12-05 11:03:46 +0100

    Update: restart_services.sh

--- restart_services.sh
+++ restart_services.sh
@@ -11,12 +11,16 @@ git pull || { echo "Failed to pull the latest changes."; exit 1; }
 echo "Activating the Python virtual environment..."
 source venv/bin/activate || { echo "Failed to activate the virtual environment."; exit 1; }
 
+# Step 3.1: Install the required Python packages
+echo "Installing the required Python packages..."
+pip install -r requirements.txt || { echo "Failed to install the required Python packages."; exit 1; }
+
 # Step 4: Run database migrations
 echo "Running database migrations..."
 python manage.py migrate || { echo "Failed to run database migrations."; exit 1; }
 
 # Step 5: Restart services
-SERVICES=("platform.service" "platform_celery.service" "platform_beat.service")
+SERVICES=("platform.service" "platform_celery.service" "platform_beat.service", "platform_flower.service")
 
 for SERVICE in "${SERVICES[@]}"; do
     echo "Restarting $SERVICE..."
